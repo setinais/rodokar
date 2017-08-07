878ce06884fb328e51a61a7e103ecc963c03108c
Setinais13 <vynny.cg@gmail.com>
2017-08-07 20:40:05 -0300

    Add funcionabilidade da espanglish

---
+++ app/controllers/EspanglishController.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+* 
+*/
+class EspanglishController extends \HXPHP\System\Controller
+{
+	public function wsAction()
+	{
+		$this->view->setTemplate(false);
+		echo "success";
+	} 
+}
